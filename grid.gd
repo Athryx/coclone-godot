7a2a59290c0f1e760628ccc6de4dd02ab1d11482
jack <jack.x.roscoe@gmail.com>
2022-07-14 12:08:01 -0500

    Switched to using more configurable multimesh based grid.

---
+++ grid.gd
@@ -0,0 +1,26 @@
+extends MultiMeshInstance
+
+export var grid_size := 11
+export var grid_opacity_fallof := 0.7
+
+func _ready():
+	material_override.set_shader_param("opacity_fallof", grid_opacity_fallof)
+	
+	multimesh.transform_format = MultiMesh.TRANSFORM_3D
+	multimesh.instance_count = grid_size * grid_size
+	
+	var half_grid_size := grid_size as float / 2.0
+	
+	for x in grid_size:
+		var xpos := x as float - half_grid_size + 0.5
+		
+		for z in grid_size:
+			var zpos := z as float - half_grid_size + 0.5
+			var index: int = z + x * grid_size
+			
+			var pos := Vector3(xpos, 0.0, zpos)
+			var instance_transform := Transform(Basis(), pos)
+			print(instance_transform.origin)
+			multimesh.set_instance_transform(index, instance_transform)
+	
+	print(multimesh.get_instance_transform(0).origin)
